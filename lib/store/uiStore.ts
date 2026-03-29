0e092b2ad57f840e1d05cf2cf0e88ffdacfc8805
36alma <85577434+36alma@users.noreply.github.com>
2026-03-29 16:37:55 +0200

    feat: add authentication constants, cookie utilities, UI store, and i18n message files

---
+++ lib/store/uiStore.ts
@@ -0,0 +1,15 @@
+﻿'use client';
+
+import {create} from 'zustand';
+
+interface UiState {
+  mobileSidebarOpen: boolean;
+  toggleSidebar: () => void;
+  setSidebarOpen: (open: boolean) => void;
+}
+
+export const useUiStore = create<UiState>((set) => ({
+  mobileSidebarOpen: false,
+  toggleSidebar: () => set((state) => ({mobileSidebarOpen: !state.mobileSidebarOpen})),
+  setSidebarOpen: (open) => set({mobileSidebarOpen: open})
+}));

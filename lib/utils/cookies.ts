19d3118e2cf0f6dbce78f8f6343f740d30de7bb2
36alma <85577434+36alma@users.noreply.github.com>
2026-03-29 16:38:01 +0200

    feat: initialize authentication constants, cookie utilities, and localization files

---
+++ lib/utils/cookies.ts
@@ -0,0 +1,6 @@
+﻿import {cookies} from 'next/headers';
+import {ACCESS_COOKIE, REFRESH_COOKIE} from './constants';
+
+export const getServerAccessToken = () => cookies().get(ACCESS_COOKIE)?.value;
+
+export const getServerRefreshToken = () => cookies().get(REFRESH_COOKIE)?.value;

4548375d9b1de2a044fd96ae073f3cb80938f838
36alma <85577434+36alma@users.noreply.github.com>
2026-03-29 19:24:51 +0200

    feat: add server-side cookie access utilities and initialize English and Hungarian localization files

--- lib/utils/cookies.ts
+++ lib/utils/cookies.ts
@@ -1,6 +1,6 @@
-﻿import {cookies} from 'next/headers';
+import {cookies} from 'next/headers';
 import {ACCESS_COOKIE, REFRESH_COOKIE} from './constants';
 
-export const getServerAccessToken = () => cookies().get(ACCESS_COOKIE)?.value;
+export const getServerAccessToken = async () => (await cookies()).get(ACCESS_COOKIE)?.value;
 
-export const getServerRefreshToken = () => cookies().get(REFRESH_COOKIE)?.value;
+export const getServerRefreshToken = async () => (await cookies()).get(REFRESH_COOKIE)?.value;

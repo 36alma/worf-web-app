33721d81f72111a5a9bc27acc655c59ded8421a0
36alma <85577434+36alma@users.noreply.github.com>
2026-04-01 13:53:43 +0200

    feat: implement core authentication, user management, and administrative group modules

--- lib/utils/cookies.ts
+++ lib/utils/cookies.ts
@@ -1,6 +1,8 @@
 import {cookies} from 'next/headers';
-import {ACCESS_COOKIE, REFRESH_COOKIE} from './constants';
+import {ACCESS_COOKIE, MFA_COOKIE, REFRESH_COOKIE} from './constants';
 
 export const getServerAccessToken = async () => (await cookies()).get(ACCESS_COOKIE)?.value;
 
 export const getServerRefreshToken = async () => (await cookies()).get(REFRESH_COOKIE)?.value;
+
+export const getServerMfaToken = async () => (await cookies()).get(MFA_COOKIE)?.value;

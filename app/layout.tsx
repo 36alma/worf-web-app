31a9283bb324e7e0618a03f147aec60ede343ab0
36alma <85577434+36alma@users.noreply.github.com>
2026-04-02 22:53:55 +0200

    feat: implement core UI components, authentication, and profile management modules

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html suppressHydrationWarning>
+    <html suppressHydrationWarning data-theme="dark">
       <body>{children}</body>
     </html>
   );

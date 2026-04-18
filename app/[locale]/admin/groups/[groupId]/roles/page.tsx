9f671fe4bd15900f34288ffc4c8d6dd59cb09b11
36alma <85577434+36alma@users.noreply.github.com>
2026-04-18 19:58:37 +0200

    feat: add admin group roles route alias

---
+++ app/[locale]/admin/groups/[groupId]/roles/page.tsx
@@ -0,0 +1,19 @@
+import GroupRolesManager from '@/components/admin/GroupRolesManager';
+import {normalizeGroupId} from '@/lib/utils/groupId';
+
+export default async function GroupRolesPage({
+  params
+}: {
+  params: Promise<{groupId: string}>;
+}) {
+  const {groupId: rawGroupId} = await params;
+  const groupId = normalizeGroupId(rawGroupId);
+
+  return (
+    <div className="h-full w-full">
+      <div className="space-y-4">
+        <GroupRolesManager groupId={groupId} />
+      </div>
+    </div>
+  );
+}

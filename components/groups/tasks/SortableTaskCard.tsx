1bb9de99c220a39353343e5df441085503bd34b6
36alma <85577434+36alma@users.noreply.github.com>
2026-04-18 19:58:56 +0200

    feat: add sortable task card

---
+++ components/groups/tasks/SortableTaskCard.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {useSortable} from '@dnd-kit/sortable';
+import {CSS} from '@dnd-kit/utilities';
+import TaskCard from './TaskCard';
+import {Task} from './types';
+
+interface SortableTaskCardProps {
+  task: Task;
+  permissions: any;
+  onClick: () => void;
+  isSelected?: boolean;
+  onToggleSelection?: () => void;
+  onModifySummary?: (newSummary: string) => void;
+}
+
+export default function SortableTaskCard({
+  task,
+  permissions,
+  onClick,
+  isSelected,
+  onToggleSelection,
+  onModifySummary
+}: SortableTaskCardProps) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: task.task_id,
+    data: {
+      type: 'Task',
+      task,
+    },
+    disabled: !permissions.task.modify,
+  });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+    opacity: isDragging ? 0.4 : undefined,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style} {...attributes}>
+      <TaskCard
+        task={task}
+        permissions={permissions}
+        onClick={onClick}
+        dragListeners={listeners}
+        isDragging={isDragging}
+        isSelected={isSelected}
+        onToggleSelection={onToggleSelection}
+        onModifySummary={onModifySummary}
+      />
+    </div>
+  );
+}
